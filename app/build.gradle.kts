4b633ed451feca2725f22e297fe58f6f3712142e
evanadwyer <evanadwyer@pronoid-software.com>
2024-11-14 10:14:57 -0800

    update compile sdk and compose BOM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.pronoidsoftware.snoozeloo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.pronoidsoftware.snoozeloo"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
